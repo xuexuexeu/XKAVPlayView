8723890640a605e948f27323dcb9977f2d7514d9
wpj <wangpengjieem@163.com>
2019-02-18 13:07:34 +0800

    Initial Commit

---
+++ XKAVplayView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XKAVplayView
+//
+//  Created by apple on 2019/2/18.
+//  Copyright © 2019年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ XKAVplayView/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  XKAVplayView
+//
+//  Created by apple on 2019/2/18.
+//  Copyright © 2019年 apple. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
